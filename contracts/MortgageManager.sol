85109c536c28b90190bf207c785d8f557a3bbab3
rotcivegaf <victorfage@gmail.com>
2018-07-19 18:34:30 -0300

    change for consume less gas

--- contracts/MortgageManager.sol
+++ contracts/MortgageManager.sol
@@ -316,7 +316,7 @@ contract MortgageManager is Cosigner, ERC721Base, ERCLockable, BytesUtils {
         unlockERC20(mana, mortgage.deposit);
         
         // Cosign contract, 0 is the RCN required
-        require(mortgage.engine.cosign(index, 0), "Error performing cosign");
+        require(engine.cosign(index, 0), "Error performing cosign");
         
         // Save mortgage id registry
         mortgageByLandId[mortgage.landId] = uint256(readBytes32(data, 0));
