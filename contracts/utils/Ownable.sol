2014e000109cb378af322c12e2541325e462b4a2
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-21 02:02:26 +0200

    Updated ownable

--- contracts/utils/Ownable.sol
+++ contracts/utils/Ownable.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.4.24;
 contract Ownable {
     address public owner;
 
+    event SetOwner(address _owner);
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Sender not owner");
         _;
     }
 
-    function Ownable() public {
-        owner = msg.sender; 
+    constructor() public {
+        owner = msg.sender;
+        emit SetOwner(msg.sender);
     }
 
     /**
@@ -17,9 +20,10 @@ contract Ownable {
 
         @param _to Address of the new owner
     */
-    function transferTo(address _to) public onlyOwner returns (bool) {
-        require(_to != address(0));
+    function setOwner(address _to) external onlyOwner returns (bool) {
+        require(_to != address(0), "Owner can't be 0x0");
         owner = _to;
+        emit SetOwner(_to);
         return true;
     } 
 } 
