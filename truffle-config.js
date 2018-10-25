68a6d55743970941164177db09015a30f3a59ba8
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-25 23:21:03 +0200

    Fix tests

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  networks: {
-    development: {
-        host: 'localhost',
-        port: 8545,
-        network_id: '*', // Match any network id,
-        gas: 47123880,
-        gasPrice: 65000000000,
-    },
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 200,
-        },
-    },
+    networks: {
+          test: {
+              host: 'localhost',
+              port: 8545,
+              network_id: '*', // Match any network id,
+              gas: 47123880,
+              gasPrice: 65000000000,
+          },
+      },
+      solc: {
+          optimizer: {
+              enabled: true,
+              runs: 200,
+          },
+      }
   }
-};
+  
